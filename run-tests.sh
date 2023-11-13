24072c2e7dada45094e6961e94927e701fe29a44
Zike Yang <zike@apache.org>
2023-11-13 11:20:23 +0800

    fix(ci): don't use color in test script echo

--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,9 @@
 bin/runtime.sh conf/contube.yaml examples/*.yaml
 
 if ! diff examples/source.txt examples/test-result-sink.txt > /tmp/contube_diff_output.txt; then
-    echo "\033[0;31mTest failed: The sink content is different, see differences below:\033[0m"
+    echo "Test failed: The sink content is different, see differences below:"
     cat /tmp/contube_diff_output.txt
     exit 1
 fi
 
-echo "\033[0;32mTest passed!\033[0m"
+echo "Test passed!"
