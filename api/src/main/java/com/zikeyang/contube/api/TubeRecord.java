00f1fc8206a3bdaf2f565e351176a4f5ada76cf6
Zike Yang <zike@apache.org>
2023-11-19 23:20:47 +0800

    feat(api): support schema in TubeRecord

--- api/src/main/java/com/zikeyang/contube/api/TubeRecord.java
+++ api/src/main/java/com/zikeyang/contube/api/TubeRecord.java
@@ -1,7 +1,13 @@
 package com.zikeyang.contube.api;
 
+import java.util.Optional;
+
 public interface TubeRecord {
   public static final TombstoneRecord TOMBSTONE_RECORD = new TombstoneRecord();
 
   byte[] getValue();
+
+  default Optional<byte[]> getSchemaData() {
+    return Optional.empty();
+  }
 }
